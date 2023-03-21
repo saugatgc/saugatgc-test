29488ed98051220694f6ae11bf03f6272419dfb9
saugatgc <128309845+saugatgc@users.noreply.github.com>
2023-03-21 21:26:34 +0545

    websites

---
+++ script.js
@@ -0,0 +1,10 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+ function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classlist.remove("active-link");
+    }
+for(tabcontent of tabcontents){
+    tabcontent.classlist.remove("active-tab");
+} 
+}
